f5d7c0199f0dab4784ff7f49be234f95c53e046b
Marcela Silva <marcela.ops@hotmail.com>
2022-08-10 17:36:12 -0300

    exercicio 4 - HackerRanck

---
+++ 04-HR-solveMeFirst.js
@@ -0,0 +1,16 @@
+// Complete the function solveMeFirst to compute the sum of two integers.
+
+// Example 
+// a = 7
+// b = 3
+
+// Return 10
+
+let x = 7
+let y = 3
+
+const solveMeFirst = (a, b) => {
+  return (a + b);
+}
+
+console.log(`A soma é: ${solveMeFirst(x, y)}`); 
