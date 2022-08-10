f010b04c546913a75a2ad68158e528726a6d4d79
Marcela Silva <marcela.ops@hotmail.com>
2022-08-10 16:49:33 -0300

    exercicio 1

---
+++ 01-operacoesAritmeticas.js
@@ -0,0 +1,22 @@
+// EXERCÍCIO 1
+// Faça as 4 operações básica com base nos números 2 e 4:
+// Ao final de cada operação imprima na tela no seguinte formato: 
+// Soma: 2 + 4 = x
+// Subtração : 2 - 4 = x
+// Divisão: 2 / 4 = x
+// Multiplicação: 2 * 4 = x
+// ---------------------------------------------------------------
+
+let num1 = 2;
+let num2 = 4;
+
+const soma = num1 + num2;
+const subtracao = num1 - num2; 
+const divisao = num1 / num2;
+const multiplicacao = num1 * num2;
+
+
+console.log(`Soma: 2 + 4 = ${num1 + num2}`);
+console.log(`Subtracao: 2 - 4 = ${subtracao}`);
+console.log(`Divisao: 2 / 4 = ${divisao}`);
+console.log(`Multiplicacao: 2 * 4 = ${multiplicacao}`);
