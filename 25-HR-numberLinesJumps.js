ece0ffe73bbad207e915b8b796cd68912e8de097
Marcela Silva <marcela.ops@hotmail.com>
2022-09-29 17:02:13 -0300

    exercicio 25

---
+++ 25-HR-numberLinesJumps.js
@@ -0,0 +1,21 @@
+// Enunciado: https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=false
+
+function kangaroo(x1, v1, x2, v2) {
+  let location1 = x1;
+  let location2 = x2;
+  let sameLocation = 'NO';
+
+  if (x2 > x1 && v2 > v1) sameLocation = 'NO';
+
+  for (let index = 0; index < 10000; index += 1) {
+    location1 += v1;
+    location2 += v2;
+
+    if (location1 === location2) sameLocation = 'YES';
+  }
+
+  return sameLocation;
+}
+
+// console.log(kangaroo(2, 1, 1, 2));
+console.log(kangaroo(0, 2, 5, 3));
